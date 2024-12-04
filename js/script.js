c5a8110e07f031c0fe580dc6cd7f67f5400fb44f
Jenna Steffes <168142203+jesteffes@users.noreply.github.com>
2024-12-04 15:02:38 -0800

    Fetch repo data

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // targeting profile info
 const overview = document.querySelector(".overview");
 const username = "jesteffes";
+const repoList = document.querySelector(".repo-list");
 
 const gitProfileInfo = async function () {
     const profileInfo = await fetch(`https://api.github.com/users/${username}`);
@@ -26,4 +27,21 @@ const displayProfileInfo = function (data) {
     </div> 
     `;
     overview.append(div);
+    gitHubRepos();
+};
+
+const gitHubRepos = async function () {
+    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoInfo = await fetchRepos.json();
+    displayRepos(repoInfo);
+    //console.log(repoInfo);
+};
+
+const displayRepos = function (repos) {
+    for (const repo of repos) {
+        const repoFeature = document.createElement("li");
+        repoFeature.classList.add("repo");
+        repoFeature.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoFeature);
+    }
 };
